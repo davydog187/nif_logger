6c2dcc3b361dc39529859290f7dc5e5f5e433d99
Dave Lucia <davelucianyc@gmail.com>
2025-10-27 19:01:53 -0400

    setup

---
+++ lib/nif_logger.ex
@@ -0,0 +1,18 @@
+defmodule NifLogger do
+  @moduledoc """
+  Documentation for `NifLogger`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NifLogger.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nif_logger_test.exs
@@ -0,0 +1,8 @@
+defmodule NifLoggerTest do
+  use ExUnit.Case
+  doctest NifLogger
+
+  test "greets the world" do
+    assert NifLogger.hello() == :world
+  end
+end
